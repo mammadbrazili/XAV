34d511a6fc1e0af4b37c014a6afe7c7c85eea4fc
mammadbrazili <168419680+mammadbrazili@users.noreply.github.com>
2024-08-05 15:47:39 +0330

    fixed multipliers

--- scoring.py
+++ scoring.py
@@ -1,117 +1,108 @@
-import os
-import gspread
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+import streamlit as st
+import streamlit as st
 import pandas as pd
 import streamlit as st
+import base64
+from io import BytesIO
 
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-def authorize_gspread(credentials_file):
-    creds = None
-    token_file = "token.json"
-    
-    if os.path.exists(token_file):
-        creds = Credentials.from_authorized_user_file(token_file, SCOPES)
+
+# Using XAV Type Face
+
+def load_black():
+    font_file_path = "xav black.ttf"
+    with open(font_file_path, "rb") as font_file:
+        font_data = font_file.read()
+    encoded_font = base64.b64encode(font_data).decode("utf-8")
+    css = f"@font-face {{ font-family: 'xav black'; src: url('data:application/octet-stream;base64,{encoded_font}'); }}"
+    st.write('<style>{}</style>'.format(css), unsafe_allow_html=True)
+
+def load_bold():
+    font_file_path = "xav semibold.ttf"
+    with open(font_file_path, "rb") as font_file:
+        font_data = font_file.read()
+    encoded_font = base64.b64encode(font_data).decode("utf-8")
+    css = f"@font-face {{ font-family: 'xav semibold'; src: url('data:application/octet-stream;base64,{encoded_font}'); }}"
+    st.write('<style>{}</style>'.format(css), unsafe_allow_html=True)
+
+load_black()
+load_bold()
+
+st.markdown("<h1 style='text-align: center; font-family: xav black;'>فرم نظرسنجی کافه</h3>", unsafe_allow_html=True)
+# Section 1
+st.markdown("<h2><div style='font-family: xav semibold; direction: ltr;'>سرو قهوه</div>", unsafe_allow_html=True)
+q1_1 = st.checkbox("تفاوت رسپی با پتانسیل", key="q1_1")
+q1_2 = st.checkbox("شناخت و پرزنت قهوه", key="q1_2")
+q1_3 = st.checkbox("استفاده از ظروف مناسب", key="q1_3")
+st.write("------------------------------------------------------------------------------------------------------")
+
+# Section 2
+st.markdown("<h2><div style='font-family: xav semibold; direction: ltr;'>باریستا</div>", unsafe_allow_html=True)
+q2_1 = st.checkbox("تئوری", key="q2_1")
+q2_2 = st.checkbox("عملی", key="q2_2")
+q2_3 = st.checkbox("پیگیری باریستا", key="q2_3")
+st.write("------------------------------------------------------------------------------------------------------")
+
+
+# Section 3
+st.markdown("<h2><div style='font-family: xav semibold; direction: ltr;'>نگهداری و نظافت</div>", unsafe_allow_html=True)
+q3_1 = st.checkbox("شرایط و نگهداری و قهوه", key="q3_1")
+q3_2 = st.checkbox("سلامت تجهیزات", key="q3_2")
+q3_3 = st.checkbox("سلامت محیط کار", key="q3_3")
+st.write("------------------------------------------------------------------------------------------------------")
+
+# Section 4
+st.markdown("<h2><div style='font-family: xav semibold; direction: ltr;'>نحوه ارائه قهوه تخصصی</div>", unsafe_allow_html=True)
+q4_1 = st.checkbox("ارزش گذاری", key="q4_1")
+q4_2 = st.checkbox("نحوه پرزنت", key="q4_2")
+
+# Define the scores for each question
+scores = {
+    "q1_1": 47, "q1_2": 47, "q1_3": 6,
+    "q2_1": 35, "q2_2": 35, "q2_3": 6,
+    "q3_1": 18, "q3_2": 61, "q3_3": 16,
+    "q4_1": 18, "q4_2": 11
+}
+
+# Calculate the total score based on selected checkboxes
+total_score = 0
+if q1_1:
+    total_score += scores["q1_1"]
+if q1_2:
+    total_score += scores["q1_2"]
+if q1_3:
+    total_score += scores["q1_3"]
+if q2_1:
+    total_score += scores["q2_1"]
+if q2_2:
+    total_score += scores["q2_2"]
+if q2_3:
+    total_score += scores["q2_3"]
+if q3_1:
+    total_score += scores["q3_1"]
+if q3_2:
+    total_score += scores["q3_2"]
+if q3_3:
+    total_score += scores["q3_3"]
+if q4_1:
+    total_score += scores["q4_1"]
+if q4_2:
+    total_score += scores["q4_2"]
+
+total_score = int(total_score/3)
+
+# Display the total score when the submit button is clicked
+if st.button("ارسال"):
+    st.subheader("نتیجه")
+    if total_score <50 :
+        st.write(f"امتیاز کل: {total_score} ")
+        st.write("V Series Cafe")
         
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
-            creds = flow.run_local_server(port=0)
-            
-        with open(token_file, "w") as token:
-            token.write(creds.to_json())
-    
-    return creds
-
-def run_sheet():
-    credentials_file = "/Users/mohammad/Sepita/credentials.json"  # Update this path to your credentials file
-    creds = authorize_gspread(credentials_file)
-    client = gspread.authorize(creds)
-    sheet = client.open_by_key("1fzCF_K8eKmKJwgcLpfQw9VVBsTdU-gL63fIEC3OxcUw").sheet1
-    return sheet
-
-def write_to_sheet(sheet, values):
-    row_count = len(sheet.get_all_values())
-    sheet.insert_row(values, row_count + 1)
-    st.success(f"Data inserted into row {row_count + 1}")
-
-def read_sheet(sheet):
-    values = sheet.get_all_values()
-    if not values:
-        st.warning("No data found in the sheet.")
-        return pd.DataFrame()
-    
-    df = pd.DataFrame(values[1:], columns=values[0])
-    return df
-
-def get_inputs():
-    name = st.text_input("نام ")
-    last_name = st.text_input("نام خانوادگی")
-    phone = st.text_input("شماره تماس ")
-    date = st.text_input("تاریخ سفارش ")
-    
-    # List of 30 options for the 'service' dropdown
-    service_options = ["پیراهن", "شلوار", "کت", "کت و شلوار", "پیراهن زنانه", "پیراهن مجلسی", "لباس رنگی", "تیشرت",
-                       "لباس زیر", "تاپ", "مانتو", "روسری و شال", "کفش و کتانی", "کیف", "کوله‌پشتی", "پتو", "ملحفه",
-                         "روتختی", "پرده", "روفرشی", "کوسن", "متکا", "چادر","چادر ماشین", "خیاطی"]
-    selected_options = st.multiselect("Select multiple options:", service_options)
-
-    selected_data = []
-
-    # Display input widgets for each selected option to get the count and price
-    if selected_options:
-        for option in selected_options:
-            count = st.number_input(f"Enter count for {option}:", min_value=0, value=1, step=1, key=f"{option}_count")
-            price = st.number_input(f"Enter price for {option}:", min_value=0.0, value=0.0, step=0.01, format="%.2f", key=f"{option}_price")
-            selected_data.append({"service": option, "count": count, "price": price})
-
-    bargain = st.text_input("تخفیف ")
-    payment_status = st.text_input("وضعیت پرداخت ")
-    order_notes = st.text_input("توضیحات مشتری ")
-    
-    return name, last_name, phone, date, selected_data, bargain, payment_status, order_notes
-
-def get_next_order_id(df):
-    if df.empty:
-        return 100
+    elif total_score > 50 and total_score<=80  :
+        st.write(f"امتیاز کل: {total_score} ")
+        st.write("A Series Cafe")
+        
     else:
-        return df["شماره سفارش"].astype(int).max() + 1
+        st.write(f"امتیاز کل: {total_score} ")
+        st.write("X Series Cafe")
 
-def main():
-    st.title("Data Entry and Display with Google Sheets")
-    # activate sheet
-    sheet = run_sheet()
-    df = read_sheet(sheet)
-    
-    # activate variables
-    name, last_name, phone, date, selected_data, bargain, payment_status, order_notes = get_inputs()
-
-    # Get the next order ID
-    order_id = get_next_order_id(df)
-
-    # Prepare new rows to be added
-    new_rows = []
-    for data in selected_data:
-        new_row = [order_id, name, last_name, phone, date, data['service'], data['count'], data['price'], bargain, payment_status, order_notes]
-        new_rows.append(new_row)
-    
-    # Convert new rows to DataFrame
-    new_df = pd.DataFrame(new_rows, columns=["شماره سفارش", "نام", "نام خانوادگی", "شماره تماس", "تاریخ سفارش", "خدمت", "تعداد", "قیمت", "تخفیف", "وضعیت پرداخت", "توضیحات مشتری"])
-    
-    # Append new rows to the existing DataFrame
-    df = pd.concat([df, new_df], ignore_index=True)
-    
-    # Display current data in the sheet
-    st.header("Current Data")
-    st.write(df)
-    
-    # Write new rows to the Google Sheet
-    for new_row in new_rows:
-        write_to_sheet(sheet, new_row)
-
-if __name__ == "__main__":
-    main()
